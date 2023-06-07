118b14d97fa23c14d7772fc788f8c8e41ed11f06
Luigiii <luigiaguado@hotmail.com>
2023-06-06 22:00:30 -0300

    Css Formulário

--- criaranuncio.php
+++ criaranuncio.php
@@ -1,7 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>TCC</title>
+</head>
+
+<body>
+    <header cl>
+        <nav class="nav-bar">
+            <div class="logo">
+            <p>
+                <a href="index.html">
+                    <img class="logotam" src="img/logo.png">
+                </a>
+            </p>
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li class="nav-item"><a href="index.html#sobretxt" class="nav-link">Sobre</a></li>
+                    <li class="nav-item"><a href="adote.html" class="nav-link">Adote</a></li>
+                    <li class="nav-item"><a href="contato.html" class="nav-link">Contato</a></li>
+                </ul>
+            </div>
+            <div class="login-button">
+                <button><a href="#">Entrar</a></button>
+            </div>
+            <div class="mobile-menu-icon">
+                <button onclick="menuShow()"><img class="icon" src="img/menubranco.svg"></button>
+            </div>
+        </nav>
+        <div class="mobile-menu">
+            <ul>
+                <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Adote</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+            </ul>
+            <div class="login-button">
+                <button><a href="#">Entrar</a></button>
+            </div>
+        </div>
+    </header>
+    <div id="contform">
 <?php
 include("config.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["acao"]) && $_POST["acao"] == "cadastrar") {
+    
     $nome = $_POST["nome"];
     $endereco = $_POST["endereco"];
     $idade = $_POST["idade"];
@@ -24,74 +72,103 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["acao"]) && $_POST["aca
     }
 }
 ?>
-
+<div id="formulario">
+    <div class="tituloform">Registrar Anúncio</div>
 <form action="?page=criaranuncio" method="POST">
     <input type="hidden" name="acao" value="cadastrar">
-    <div>
-        <label>Nome</label>
-        <input type="text" name="nome">
+    <div class="detalheform">
+    <div class="infoform">
+        <label class="letraform">Nome</label>
+        <input type="text" name="nome" placeholder="Insira o nome do animal">
     </div>
-    <div>
-        <label>Endereço</label>
-        <input type="text" name="endereco">
+    <div class="infoform">
+        <label class="letraform">Endereço</label>
+        <input type="text" name="endereco" placeholder="Insira a localização do animal">
     </div>
-    <div>
-        <label>Idade (Aproximada)</label>
-        <input type="number" name="idade" value="0">
+    <div class="infoform">
+        <label class="letraform">Idade (Aproximada)</label>
+        <input type="number" id="idadeseta" name="idade" value="0" min="0">
     </div>
-    <div>
-        <label>Sexo</label>
-        <input type="text" name="sexo">
+    <div class="infoform">
+        <label class="letraform">Sexo</label>
+        <input type="text" name="sexo" placeholder="Insira o porte do animal">
     </div>
-    <div>
-        <label>Porte</label>
-        <input type="text" name="porte">
+    <div class="infoform">
+        <label class="letraform">Porte</label>
+        <input type="text" name="porte" placeholder="Insira o porte do animal">
     </div>
-    <div>
-        <label>Raça</label>
-        <input type="text" name="raca">
+    <div class="infoform">
+        <label class="letraform">Raça</label>
+        <input type="text" name="raca" placeholder="Insira a raça do animal">
     </div>
-    <div>
-        <label>Cor</label>
-        <input type="text" name="cor">
+    <div class="infoform">
+        <label class="letraform">Cor</label>
+        <input type="text" name="cor" placeholder="Insira a cor do animal">
     </div>
-    <div>
-        <label>Enfermidade</label>
-        <input type="text" name="enfermidade">
+    <div class="infoform">
+        <label class="letraform">Enfermidade</label>
+        <input type="text" name="enfermidade" placeholder="Insira a enfermidade do animal">
     </div>
+    </div>
+    <div id="fotoanimalform">
     <div>
         <label>Foto do animal</label>
         <input type="file" name="fotoanuncio" id="fotoanuncio" accept="image/*" required>
     </div>
+    </div>
     <div>
-        <button type="submit">Criar</button>
+        <button id="buttoncriar" type="submit">Criar</button>
     </div>
 </form>
-
-<div>
-    <?php
-    $sql = "SELECT * FROM anuncio";
-
-    $res = $conn->query($sql);
-
-    $qtd = $res->num_rows;
-
-    if ($qtd > 0) {
-        while ($row = $res->fetch_object()) {
-            print $row->nome;
-            print $row->endereco;
-            print $row->idade;
-            print $row->sexo;
-            print $row->porte;
-            print $row->raca;
-            print $row->cor;
-            print $row->enfermidade;
-            print $row->fotoanuncio;
-            echo "<button onclick=\"location.href='editarusuario.php?id=".$row->id."';\">Editar</button>";
-            echo "<button onclick=\"if(confirm('Tem certeza que deseja excluir o anúncio?')){location.href='exluiranuncio.php?id=".$row->id."';}else{false;}\">Excluir</button>";
-        }
-    } else {
-        print "<p>Não existe anúncios</p>";
-    }
-    ?>
 </div>
+<div>
+</div>
+</div>
+</body>
+<footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <p>
+                    <a href="index.html">
+                        <img class="logofootertam" src="img/logo.png">
+                    </a>
+                </p>
+            </div>
+
+            <div id="footer_social_media">
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                </div>
+
+            <div id="footer_subscribe">
+                <h3>Dúvidas</h3>
+
+                <p>
+                    Caso tenha alguma pergunta, clique no botão a baixo.
+                </p>
+
+                    <button id="duvidasbutton">
+                        <i class="fa-solid fa-question"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer_copyright">
+            &#169
+            2023 all rights reserved
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
--- editarusuario.php
+++ editarusuario.php
@@ -1,3 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>TCC</title>
+</head>
+
+<body>
+    <header cl>
+        <nav class="nav-bar">
+            <div class="logo">
+            <p>
+                <a href="index.html">
+                    <img class="logotam" src="img/logo.png">
+                </a>
+            </p>
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li class="nav-item"><a href="index.html#sobretxt" class="nav-link">Sobre</a></li>
+                    <li class="nav-item"><a href="adote.html" class="nav-link">Adote</a></li>
+                    <li class="nav-item"><a href="contato.html" class="nav-link">Contato</a></li>
+                </ul>
+            </div>
+            <div class="login-button">
+                <button><a href="#">Entrar</a></button>
+            </div>
+            <div class="mobile-menu-icon">
+                <button onclick="menuShow()"><img class="icon" src="img/menubranco.svg"></button>
+            </div>
+        </nav>
+        <div class="mobile-menu">
+            <ul>
+                <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Adote</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+            </ul>
+            <div class="login-button">
+                <button><a href="#">Entrar</a></button>
+            </div>
+        </div>
+    </header>
+    <div id="contform">
 <?php
     include ("config.php");
     // Verificar se o parâmetro "id" está definido e não vazio
@@ -40,47 +87,102 @@
         echo "ID do anúncio não especificado.";
     }
 ?>
-
+<div id="formulario">
+<div class="tituloform">Atualizar Anúncio</div>
 <form action="?page=editaranuncio&id=<?php echo $_REQUEST["id"]; ?>" method="POST">
     <input type="hidden" name="acao" value="editar">
     <input type="hidden" name="id" value="<?php echo $row->id; ?>">
-    <div>
-        <label>Nome</label>
+    <div class="detalheform">
+    <div class="infoform">
+        <label class="letraform">Nome</label>
         <input type="text" name="nome" value="<?php echo $row->nome; ?>">
     </div>
-    <div>
-        <label>Endereço</label>
+    <div class="infoform">
+        <label class="letraform">Endereço</label>
         <input type="text" name="endereco" value="<?php echo $row->endereco; ?>">
     </div>
-    <div>
-        <label>Idade (Aproximada)</label>
-        <input type="number" name="idade" value="<?php echo $row->idade; ?>">
+    <div class="infoform">
+        <label class="letraform">Idade (Aproximada)</label>
+        <input type="number" id="idadeseta" name="idade" value="<?php echo $row->idade; ?>">
     </div>
-    <div>
-        <label>Sexo</label>
+    <div class="infoform">
+        <label class="letraform">Sexo</label>
         <input type="text" name="sexo" value="<?php echo $row->sexo; ?>">
     </div>
-    <div>
-        <label>Porte</label>
+    <div class="infoform">
+        <label class="letraform">Porte</label>
         <input type="text" name="porte" value="<?php echo $row->porte; ?>">
     </div>
-    <div>
-        <label>Raça</label>
+    <div class="infoform">
+        <label class="letraform">Raça</label>
         <input type="text" name="raca" value="<?php echo $row->raca; ?>">
     </div>
-    <div>
-        <label>Cor</label>
+    <div class="infoform">
+        <label class="letraform">Cor</label>
         <input type="text" name="cor" value="<?php echo $row->cor; ?>">
     </div>
-    <div>
-        <label>Enfermidade</label>
+    <div class="infoform">
+        <label class="letraform">Enfermidade</label>
         <input type="text" name="enfermidade" value="<?php echo $row->enfermidade; ?>">
     </div>
+    </div>
+    <div id="fotoanimalform">
     <div>
         <label>Foto do animal</label>
-        <input type="file" name="fotoanuncio" id="fotoanuncio" accept="image/*" required>
+        <input type="file" name="fotoanuncio" id="fotoanuncio" required>
+    </div>
     </div>
     <div>
-        <button type="submit">Editar</button>
+        <button id="buttoncriar" type="submit">Editar</button>
     </div>
 </form>
+</div>
+</div>
+</body>
+<footer>
+        <div id="footer_content">
+            <div id="footer_contacts">
+                <p>
+                    <a href="index.html">
+                        <img class="logofootertam" src="img/logo.png">
+                    </a>
+                </p>
+            </div>
+
+            <div id="footer_social_media">
+                    <a href="#" class="footer-link" id="instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="facebook">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+
+                    <a href="#" class="footer-link" id="whatsapp">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                </div>
+
+            <div id="footer_subscribe">
+                <h3>Dúvidas</h3>
+
+                <p>
+                    Caso tenha alguma pergunta, clique no botão a baixo.
+                </p>
+
+                    <button id="duvidasbutton">
+                        <i class="fa-solid fa-question"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer_copyright">
+            &#169
+            2023 all rights reserved
+        </div>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
