8b6de8a4913fc101c9e40085a0f4e1ee3fc78e5d
gabrielesperanca2002 <gabrielesperanca2002@gmail.com>
2023-10-28 21:36:00 -0300

    Filtro atualizado

--- adote.php
+++ adote.php
@@ -1,7 +1,7 @@
 <?php
 include("config.php");
-
 include("header.php");
+
 if(!isset($_SESSION['id'])) {
     header("Location: escolhalogin.php");
 }
@@ -11,6 +11,17 @@ if(!isset($_SESSION['id'])) {
     <div>
         <input type="text" id="nome_animal" placeholder="Pesquisar por nome">
         <input type="text" id="endereco_animal" placeholder="Pesquisar por endereço">
+        <label for="sexo">Sexo:</label>
+        <input type="radio" name="sexo" id="sexo_macho" value="Macho"> Macho
+        <input type="radio" name="sexo" id="sexo_femea" value="Fêmea"> Fêmea
+        <select id="porte">
+            <option value="">Selecione o porte</option>
+            <option value="Pequeno">Pequeno</option>
+            <option value="Médio">Médio</option>
+            <option value="Grande">Grande</option>
+        </select>
+        <input type="text" id="raca" placeholder="Pesquisar por raça">
+        <input type="text" id="enfermidade" placeholder="Pesquisar por enfermidade">
         <button onclick="filtrarAnimais()">Pesquisar</button>
     </div>
     <div id="adotetudo">
@@ -22,14 +33,16 @@ if(!isset($_SESSION['id'])) {
         if ($qtd > 0) {
             while ($row = $res->fetch_object()) {
         ?>
-                <div id="adoteall">
-                    <div class="cardadote">
-                        <a onclick="location.href='perfildog.php?id=<?php echo $row->id; ?>';">
-                            <img src="img/<?php echo $row->fotoanuncio; ?>" width="130">
-                            <h2><?php echo $row->nome; ?></h2>
-                            <h3><?php echo $row->endereco; ?></h3>
-                        </a>
-                    </div>
+                <div class="cardadote">
+                    <a onclick="location.href='perfildog.php?id=<?php echo $row->id; ?>';">
+                        <img src="img/<?php echo $row->fotoanuncio; ?>" width="130">
+                        <h2><?php echo $row->nome; ?></h2>
+                        <h3><?php echo $row->endereco; ?></h3>
+                        <p class="sexo"><?php echo $row->sexo; ?></p>
+                        <p class="porte"><?php echo $row->porte; ?></p>
+                        <p class="raca"><?php echo $row->raca; ?></p>
+                        <p class="enfermidade"><?php echo $row->enfermidade; ?></p>
+                    </a>
                 </div>
         <?php
             }
@@ -44,16 +57,29 @@ if(!isset($_SESSION['id'])) {
     function filtrarAnimais() {
         var nomeAnimal = document.getElementById("nome_animal").value.toLowerCase();
         var enderecoAnimal = document.getElementById("endereco_animal").value.toLowerCase();
+        var sexoAnimal = document.querySelector('input[name="sexo"]:checked') ? document.querySelector('input[name="sexo"]:checked').value : "";
+        var porteAnimal = document.getElementById("porte").value.toLowerCase();
+        var racaAnimal = document.getElementById("raca").value.toLowerCase();
+        var enfermidadeAnimal = document.getElementById("enfermidade").value.toLowerCase();
         var cards = document.querySelectorAll(".cardadote");
 
         for (var i = 0; i < cards.length; i++) {
             var animalNome = cards[i].getElementsByTagName("h2")[0].innerText.toLowerCase();
             var animalEndereco = cards[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+            var animalSexo = cards[i].querySelector('.sexo').innerText.toLowerCase();
+            var animalPorte = cards[i].querySelector('.porte').innerText.toLowerCase();
+            var animalRaca = cards[i].querySelector('.raca').innerText.toLowerCase();
+            var animalEnfermidade = cards[i].querySelector('.enfermidade').innerText.toLowerCase();
 
-            if (animalNome.includes(nomeAnimal) && animalEndereco.includes(enderecoAnimal)) {
-                cards[i].parentElement.style.display = "block";
+            if (animalNome.includes(nomeAnimal) &&
+                animalEndereco.includes(enderecoAnimal) &&
+                (sexoAnimal === "" || animalSexo.includes(sexoAnimal)) &&
+                (porteAnimal === "" || animalPorte.includes(porteAnimal)) &&
+                animalRaca.includes(racaAnimal) &&
+                animalEnfermidade.includes(enfermidadeAnimal)) {
+                cards[i].style.display = "block";
             } else {
-                cards[i].parentElement.style.display = "none";
+                cards[i].style.display = "none";
             }
         }
     }
@@ -61,5 +87,4 @@ if(!isset($_SESSION['id'])) {
 
 <?php
 include("footer.php");
-
 ?>
