bd2d2c89198de418e8ea19b6c8fc53a85b56d61d
Luigiii <luigiaguado@hotmail.com>
2023-11-28 19:35:55 -0300

    Ajustes

--- exluiranuncio.php
+++ exluiranuncio.php
@@ -1,17 +1,16 @@
-<!-- exluiranuncio.php -->
 <?php
     include("config.php");
 
-    // Verificar se o parâmetro "id" está definido
+
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
 
-        // Verificar se o anúncio existe no banco de dados
+
         $sql = "SELECT * FROM anuncio WHERE id = $id";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
-            // Anúncio encontrado, realizar a exclusão
+
             $deleteSql = "DELETE FROM anuncio WHERE id = $id";
             if ($conn->query($deleteSql) === true) {
                 echo "Anúncio excluído com sucesso.";
